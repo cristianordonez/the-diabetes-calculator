62ebd5817a7a9b588efb97d1042fb5d6030615d0
Cristian Ordonez <cristianordonezrd@gmail.com>
2022-10-19 18:30:25 -0400

    fixed spacing issue with nutrition table

--- client/src/components/nutrition-table/NutritionTable.tsx
+++ client/src/components/nutrition-table/NutritionTable.tsx
@@ -27,35 +27,33 @@ export const NutritionTable = ({
       <TableRow>
          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
             <Collapse in={open} timeout='auto' unmountOnExit>
-               <Box sx={{ margin: 1 }}>
-                  <>
-                     <Typography variant='h6' gutterBottom component='div'>
-                        Nutrition Facts
-                     </Typography>
-                     <Table
-                        size='small'
-                        aria-label='additional nutrition data'
-                        stickyHeader={true}
-                     >
-                        <TableHead>
-                           <TableRow>
-                              <StyledTableCell>Nutrient</StyledTableCell>
-                              <StyledTableCell>Amount</StyledTableCell>
-                           </TableRow>
-                        </TableHead>
-                        <TableBody>
-                           {Object.entries(nutrition).map((nutrient) => (
-                              <NutritionTableRow
-                                 nutrientName={nutrient[0]}
-                                 nutrientAmount={nutrient[1]}
-                                 serving_size_conversion_factor={
-                                    serving_size_conversion_factor
-                                 }
-                              />
-                           ))}
-                        </TableBody>
-                     </Table>
-                  </>
+               <Box sx={{ margin: 1, minWidth: { xs: '0', sm: '400px' } }}>
+                  <Typography variant='h6' gutterBottom component='div'>
+                     Nutrition Facts
+                  </Typography>
+                  <Table
+                     size='small'
+                     aria-label='additional nutrition data'
+                     stickyHeader={true}
+                  >
+                     <TableHead>
+                        <TableRow>
+                           <StyledTableCell>Nutrient</StyledTableCell>
+                           <StyledTableCell>Amount</StyledTableCell>
+                        </TableRow>
+                     </TableHead>
+                     <TableBody>
+                        {Object.entries(nutrition).map((nutrient) => (
+                           <NutritionTableRow
+                              nutrientName={nutrient[0]}
+                              nutrientAmount={nutrient[1]}
+                              serving_size_conversion_factor={
+                                 serving_size_conversion_factor
+                              }
+                           />
+                        ))}
+                     </TableBody>
+                  </Table>
                </Box>
             </Collapse>
          </TableCell>
